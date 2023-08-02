b17f34b22872c39e830e39e932f7b9fea554297e
Henrique_Programmer <130008663+MarceloHenrique7@users.noreply.github.com>
2023-08-02 13:27:07 -0300

    a

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Marcelo Henrique'
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Marcelo Henrique'
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
-
